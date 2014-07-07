03e22496484c9943834402137151292a430634ab
akamensky <me@akamensky.com>
2014-07-07 19:29:48 +0800

    Updated Jetty version to latest

--- src/main/java/xpressj/JettyLogger.java
+++ src/main/java/xpressj/JettyLogger.java
@@ -32,6 +32,7 @@ public class JettyLogger implements Logger {
     @Override public boolean isDebugEnabled() { return false; }
     @Override public void setDebugEnabled(boolean enabled) { }
     @Override public void debug(String msg, Object... args) { }
+    @Override public void debug(String s, long l) {}
     @Override public void debug(Throwable thrown) { }
     @Override public void debug(String msg, Throwable thrown) { }
     @Override public Logger getLogger(String name) { return this; }

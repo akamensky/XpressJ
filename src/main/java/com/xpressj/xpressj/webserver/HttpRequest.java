e612ee895dbef9b629b1d7ee4fae714c31c95d5f
akamensky <me@akamensky.com>
2014-07-15 17:52:39 +0800

    Request/Response objects

---
+++ src/main/java/com/xpressj/xpressj/webserver/HttpRequest.java
@@ -0,0 +1,14 @@
+package com.xpressj.xpressj.webserver;
+
+import java.net.Socket;
+
+/**
+ * Created by akamensky on 7/15/14.
+ */
+public class HttpRequest {
+
+    public HttpRequest(Socket socket){
+        //Parse request
+    }
+
+}
---
+++ src/main/java/com/xpressj/xpressj/webserver/HttpResponse.java
@@ -0,0 +1,7 @@
+package com.xpressj.xpressj.webserver;
+
+/**
+ * Created by akamensky on 7/15/14.
+ */
+public class HttpResponse {
+}

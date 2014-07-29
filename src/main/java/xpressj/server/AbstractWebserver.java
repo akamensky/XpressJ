963b72bb5d27025031101322402d7f2d04746353
akamensky <me@akamensky.com>
2014-07-29 19:11:56 +0800

    Abstract class representing webserver

---
+++ src/main/java/xpressj/server/AbstractWebserver.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 - Alexey Kamenskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xpressj.server;
+
+import xpressj.Configuration;
+
+/**
+ * Created by akamensky on 7/29/14.
+ */
+public abstract class AbstractWebserver {
+
+    /**
+     * Configuration class object
+     * that contains needed configurations
+     */
+    protected final Configuration configuration;
+
+    public AbstractWebserver(Configuration configuration){
+        this.configuration = configuration;
+    };
+
+    /**
+     * Suppose to start web server using configuration
+     */
+    public abstract void start();
+
+    /**
+     * Suppose to stop web server (app should exit if no other server running)
+     */
+    public abstract void stop();
+
+    /**
+     * Suppose to restart web server (without exiting app)
+     */
+    public abstract void restart();
+
+}

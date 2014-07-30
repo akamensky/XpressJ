61baff69e2031ec1038030726874674a7369776f
akamensky <me@akamensky.com>
2014-07-30 20:50:46 +0800

    Final changes + code reformatting

---
+++ src/main/java/xpressj/server/CookieImpl.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2014 - Alexey Kamenskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xpressj.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by akamensky on 7/30/14.
+ */
+public class CookieImpl implements Cookie {
+
+    private static final String DEFAULT_PATH = "/";
+    private static final int DEFAULT_MAXAGE = 0;
+    private static final boolean DEFAULT_ISSECURE = false;
+    private static final boolean DEFAULT_ISHTTPONLY = false;
+
+    private String name;
+    private String value;
+    private String path;
+    private int maxAge;
+    private boolean isSecure;
+    private boolean isHttpOnly;
+
+    public CookieImpl(javax.servlet.http.Cookie httpCookie) {
+        this.name = httpCookie.getName();
+        this.value = httpCookie.getValue();
+        this.path = httpCookie.getPath();
+        this.maxAge = httpCookie.getMaxAge();
+        this.isSecure = httpCookie.getSecure();
+        this.isHttpOnly = httpCookie.isHttpOnly();
+    }
+
+    public CookieImpl(String name, String value) {
+        this(name, value, DEFAULT_MAXAGE, DEFAULT_PATH, DEFAULT_ISSECURE, DEFAULT_ISHTTPONLY);
+    }
+
+    public CookieImpl(String name, String value, int maxAge) {
+        this(name, value, maxAge, DEFAULT_PATH, DEFAULT_ISSECURE, DEFAULT_ISHTTPONLY);
+    }
+
+    public CookieImpl(String name, String value, int maxAge, String path) {
+        this(name, value, maxAge, path, DEFAULT_ISSECURE, DEFAULT_ISHTTPONLY);
+    }
+
+    public CookieImpl(String name, String value, int maxAge, String path, boolean isSecure) {
+        this(name, value, maxAge, path, isSecure, DEFAULT_ISHTTPONLY);
+    }
+
+    public CookieImpl(String name, String value, int maxAge, String path, boolean isSecure, boolean isHttpOnly) {
+        this.name = name;
+        this.value = value;
+        this.path = path;
+        this.maxAge = maxAge;
+        this.isSecure = isSecure;
+        this.isHttpOnly = isHttpOnly;
+    }
+
+    public static Map<String, Cookie> toMap(javax.servlet.http.Cookie[] cookies) {
+        Map<String, Cookie> map = new HashMap<String, Cookie>();
+        for (javax.servlet.http.Cookie cookie : cookies) {
+            Cookie newCookie = new CookieImpl(cookie);
+            map.put(newCookie.getName(), newCookie);
+        }
+        return map;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public javax.servlet.http.Cookie getServletCookie() {
+        javax.servlet.http.Cookie cookie = new javax.servlet.http.Cookie(this.name, this.value);
+        if (this.path != null) {
+            cookie.setPath(this.path);
+        }
+        if (this.maxAge > 0) {
+            cookie.setMaxAge(this.maxAge);
+        }
+        cookie.setHttpOnly(this.isHttpOnly);
+        cookie.setSecure(this.isSecure);
+        return cookie;
+    }
+
+    public String toString() {
+        return this.value;
+    }
+}
---
+++ src/main/java/xpressj/server/JettyServer.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2014 - Alexey Kamenskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xpressj.server;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.Resource;
+import xpressj.Configuration;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by akamensky on 7/30/14.
+ */
+public class JettyServer {
+
+    private Server server;
+    private SessionFactory sessionFactory;
+    private RequestHandler handler;
+    private Configuration configuration;
+
+    public JettyServer(Configuration configuration) {
+        this.configuration = configuration;
+        this.handler = new RequestHandler(this.configuration.getRouteMatcher());
+        org.eclipse.jetty.util.log.Log.setLog(new JettyLogger());
+    }
+
+    private static ServerConnector createSocketConnector() {
+        return new ServerConnector(new Server());
+    }
+
+    public void start(Object lock) {
+        if (this.configuration.useSessions()) {
+            this.sessionFactory = new SessionFactory(this.configuration);
+            this.handler.setSessionFactory(this.sessionFactory);
+        }
+
+        ServerConnector connector = createSocketConnector();
+
+        connector.setIdleTimeout(TimeUnit.HOURS.toMillis(1));
+        connector.setSoLingerTime(-1);
+        connector.setHost(this.configuration.getHost());
+        connector.setPort(this.configuration.getPort());
+
+        this.server = connector.getServer();
+        this.server.setConnectors(new Connector[]{connector});
+
+        this.server.setStopTimeout(0);
+
+        List<Handler> handlerList = new ArrayList<>();
+
+        //Add main handler
+        handlerList.add(this.handler);
+        //Add bundled static files handler
+        if (this.configuration.getStaticFilesLocation() != null) {
+            ResourceHandler resourceHandler = new ResourceHandler();
+            Resource staticResources = Resource.newClassPathResource(this.configuration.getStaticFilesLocation());
+            resourceHandler.setBaseResource(staticResources);
+            handlerList.add(resourceHandler);
+        }
+        //Add external static files handler
+        if (this.configuration.getExternalStaticFilesLocation() != null) {
+            ResourceHandler externalResourceHandler = new ResourceHandler();
+            Resource externalStaticResources = Resource.newResource(new File(this.configuration.getExternalStaticFilesLocation()));
+            externalResourceHandler.setBaseResource(externalStaticResources);
+            handlerList.add(externalResourceHandler);
+        }
+        //Add error handler
+        handlerList.add(new ErrorHandler(this.configuration));
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(handlerList.toArray(new Handler[handlerList.size()]));
+        this.server.setHandler(handlers);
+
+        try {
+            this.server.start();
+
+            System.out.println("** " + this.configuration.NAME + " has started ...");
+            System.out.println("** Listening on " + this.configuration.getHost() + ":" + this.configuration.getPort());
+
+            synchronized (lock) {//TODO: get rid of this somehow?
+                lock.notifyAll();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        System.out.println("** " + this.configuration.NAME + " is stopping ...");
+        try {
+            if (server != null) {
+                server.stop();
+                if (this.sessionFactory != null) {
+                    this.sessionFactory = null;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("** " + this.configuration.NAME + " has stopped ...");
+    }
+}
